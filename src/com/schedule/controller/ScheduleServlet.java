5058eb80c6c27c955bc77d75ae3f39e41a9e2fbd
DESKTOP-0U5VJ0C\willis <love0725@gmail.com>
2017-10-24 18:45:43 +0800

    會員功能完成功能調整1.加入排程器2.新增登入與修改成功頁面

---
+++ src/com/schedule/controller/ScheduleServlet.java
@@ -0,0 +1,39 @@
+package com.schedule.controller;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.sql.*;
+
+import java.util.*;
+
+public class ScheduleServlet extends HttpServlet {
+   
+    
+    Timer timer;
+    int i=0;      
+   
+    public void init() throws ServletException {
+      TimerTask task = new TimerTask(){ 
+	        public void run() {
+            System.out.println("i="+i);
+            i++;	      
+	        } 
+      };
+      timer = new Timer(); 
+      Calendar cal = new GregorianCalendar(2017, Calendar.OCTOBER, 23, 21, 19, 30);//年月日時份秒
+      timer.scheduleAtFixedRate(task, cal.getTime(), 1*60*60*1000);
+      System.out.println("已建立排程!");       
+    }
+    
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+                               throws ServletException, IOException {
+    }                           	
+
+    public void destroy() {
+        timer.cancel();
+        System.out.println("已移除排程!");
+    }
+    
+}
