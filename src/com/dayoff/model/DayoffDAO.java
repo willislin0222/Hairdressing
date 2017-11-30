03c506a8155b193f304a95c31827e62514e92c70
DESKTOP-0U5VJ0C\willis <love0725@gmail.com>
2017-11-30 10:34:30 +0800

    功能調整:1.將dayoff功能修改為Spring

--- src/com/dayoff/model/DayoffDAO.java
+++ src/com/dayoff/model/DayoffDAO.java
@@ -2,91 +2,49 @@ package com.dayoff.model;
 
 import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.query.Query;
-
-import com.member.model.MemberVO;
-
-import hibernate.util.HibernateUtil;
+import org.springframework.orm.hibernate5.HibernateTemplate;
 
 public class DayoffDAO implements DayoffDAO_interface{
 
 	private static final String GET_ALL_STMT="from DayoffVO order by day_no desc";
+	
+	//springframework hibernate5
+	private HibernateTemplate hibernateTemplate;    
+		
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) { 
+	    this.hibernateTemplate = hibernateTemplate;
+	}
 	@Override
 	public void insert(DayoffVO dayoffVO) {
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-		
-		try {
-			session.beginTransaction();
-			session.saveOrUpdate(dayoffVO);
-			session.getTransaction().commit();
-		} catch (RuntimeException e) {
-			session.getTransaction().rollback();
-			throw e;
-		}
+		hibernateTemplate.saveOrUpdate(dayoffVO);
 		
 	}
 
 	@Override
 	public void update(DayoffVO dayoffVO) {
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-		
-		try {
-			session.beginTransaction();
-			session.saveOrUpdate(dayoffVO);
-			session.getTransaction().commit();
-		} catch (RuntimeException e) {
-			session.getTransaction().rollback();
-			throw e;
-		}
+		hibernateTemplate.update(dayoffVO);
 		
 	}
 
 	@Override
 	public void delete(String day_no) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
 		
-		try {
-			session.beginTransaction();
-			DayoffVO dayoffVO =(DayoffVO) session.get(DayoffVO.class, day_no);
-			session.delete(dayoffVO);
-			session.getTransaction().commit();
-		} catch (RuntimeException e) {
-			session.getTransaction().rollback();
-			throw e;
-		}
+		DayoffVO dayoffVO =(DayoffVO) hibernateTemplate.get(DayoffVO.class, day_no);
+		hibernateTemplate.delete(dayoffVO);
 		
 	}
 
 	@Override
 	public DayoffVO findByPrimaryKey(String day_no) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		DayoffVO dayoffVO =null;
-		try {
-			session.beginTransaction();
-			dayoffVO =(DayoffVO) session.get(DayoffVO.class, day_no);
-			session.getTransaction().commit();
-		} catch (RuntimeException e) {
-			session.getTransaction().rollback();
-			throw e;
-		}
+		DayoffVO dayoffVO =(DayoffVO) hibernateTemplate.get(DayoffVO.class, day_no);
 		return dayoffVO;
 	}
 
 	@Override
 	public List<DayoffVO> getAll() {
 		List<DayoffVO> list = null;
-		Session session = HibernateUtil.getSessionFactory().openSession();
 		
-		try {
-			session.beginTransaction();
-			Query query = session.createQuery(GET_ALL_STMT);
-			list = query.list();
-			session.getTransaction().commit();
-		} catch (RuntimeException e) {
-			session.getTransaction().rollback();
-			throw e;
-		}
+		list = (List<DayoffVO>) hibernateTemplate.find(GET_ALL_STMT);
 		return list;
 	}
 
--- src/com/dayoff/model/DayoffDAO_interface.java
+++ src/com/dayoff/model/DayoffDAO_interface.java
@@ -2,6 +2,9 @@ package com.dayoff.model;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
 public interface DayoffDAO_interface {
 	public void insert(DayoffVO dayoffVO);
 	public void update(DayoffVO dayoffVO);
